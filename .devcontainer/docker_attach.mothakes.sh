f636548d2b8394bdc8b0f555d15e5310618fa8ee
Simon Fong <simonfong6@gmail.com>
2020-03-09 00:53:19 +0000

    Attach

--- .devcontainer/docker_attach.mothakes.sh
+++ .devcontainer/docker_attach.mothakes.sh
@@ -5,5 +5,5 @@ docker container run \
     --user vscode \
     --mount type=bind,source=/home/ubuntu/Projects/image-share,target=/workspace/image-share \
     --workdir /workspace/image-share \
-    --publish 3034:3034 \
+    --publish 1343:1343 \
     image-share.mothakes.com.image:latest /bin/bash && pip3 install -r requirements.txt
